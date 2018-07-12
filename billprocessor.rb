104f19eef7379d0ea6e74d1ddc62149abbcd842b
braithair <braithair@gmail.com>
2018-07-12 13:46:41 +0000

    Updating billprocessor.rb

--- billprocessor.rb
+++ billprocessor.rb
@@ -6,4 +6,22 @@
 # Define variable for directory
 root_dir = "/tmp/bills"
 
+# Simply displays the working directory for debug
 puts root_dir
+
+# Define processing directories
+import_dir = "#{root_dir}/import"
+temp_dir = "#{root_dir}/temp"
+export_dir = "#{root_dir}/export"
+
+#Display the import directory
+puts import_dir
+puts temp_dir
+puts export_dir
+
+#Direcyory check
+if Dir.exist? (ENV['/tmp/bills'])
+	puts "The root directory exists."
+else
+	puts "Cannot find the root directory."
+end
