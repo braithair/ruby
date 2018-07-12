5f28a44c66662fecbd82d8477a760d16e2e2b794
braithair <braithair@gmail.com>
2018-07-12 12:10:22 +0000

    Adding billprocessor.rb

---
+++ billprocessor.rb
@@ -0,0 +1,9 @@
+# Name:		billprocessor.rb
+# Author:	Matthew McCracken
+# Version:	1.0
+# Descriptor:	Extracts zipped CSV data, collates, then outputs data.
+
+# Define variable for directory
+root_dir = "/tmp/bills"
+
+puts root_dir
