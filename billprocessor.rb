8a33268a60b9d6a7c4f12f3c0378818a1c3a6ddb
Matthew McCracken <braithair@gmail.com>
2018-07-14 20:49:11 +1000

    Adding case details

--- billprocessor.rb
+++ billprocessor.rb
@@ -9,8 +9,6 @@ require 'rbconfig'
 # Define Operating System with namespace resolution operator (::) within rbconfig
 @os = RbConfig::CONFIG['host_os']
 
-
-
 # Begin case which will define where the root files will be found.
 # There will be cases for 3 platforms: Linux, OSX and Windows
 case
@@ -27,7 +25,14 @@ when @os.downcase.include?('linux')
 	# Descrbe Environment
 	puts "Platform is Linux, so the files will be in #{root_dir}"
 when @os.downcase.include?('darwin')
-  @os = 'osx'
+	# Define OS as OSX
+	@os = 'osx'
+
+	# Define OSX processing directories (expected to be same as Linux)
+	root_dir = "/tmp/bills"
+	import_dir = "#{root_dir}/import"
+	temp_dir = "#{root_dir}/temp"
+	export_dir = "#{root_dir}/export"
   puts 'Platform is OSX'
 else
   @os = 'windows'
@@ -35,6 +40,7 @@ else
 end
 
 #Display the import directory
+puts "Given your OS is #{@os}, your processing directories will be:"
 puts import_dir
 puts temp_dir
 puts export_dir
